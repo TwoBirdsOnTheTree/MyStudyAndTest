f0c675258bf7883a5e387eca465d8604e79f6d83
WhoKnows <784925761@qq.com>
2019-05-28 18:05:26 +0800

    typescript接口

--- TypeScriptTest/src/test.ts
+++ TypeScriptTest/src/test.ts
@@ -6,21 +6,20 @@ interface OtherPeople {
     talk(word: string): string;
 }
 
-// 鸭式辨行法
-var a = {
-    talk(word: string) {
-        return word;
-    }
-};
-
-
-
 class You implements People {
     talk(word: string): string {
         return word;
     }
 
 }
+
+// 鸭式辨行法
+// a没有指定类型
+var a = {
+    talk(word: string) {
+        return word;
+    }
+};
 
 a.talk("nihao");
 
@@ -33,5 +32,9 @@ function funOtherPeople(p: OtherPeople) {
 
 }
 
+// a可以是People类型
 fun(a);
+// a也可以是O他和人People类型
 funOtherPeople(a);
+// 不支持接口的实例化
+var you = new You();
