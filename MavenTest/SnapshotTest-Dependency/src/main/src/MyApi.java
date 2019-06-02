523c782fa1f51b1d852ffc5a408d45a58f9bf799
784925761@qq.com <2smqyV3CwgQb>
2019-06-03 00:48:46 +0800

    Maven测试

---
+++ MavenTest/SnapshotTest-Dependency/src/main/src/MyApi.java
@@ -0,0 +1,8 @@
+public class MyApi {
+    public static String getString() {
+        return "1";
+
+        // 修改返回值为2后, mvn install
+        // return "2";
+    }
+}
---
+++ MavenTest/SnapshotTest/src/main/src/SnapshotTest.java
@@ -0,0 +1,7 @@
+public class SnapshotTest {
+    public static void main(String[] args) {
+        String getFromSnapshotApi = MyApi.getString();
+
+        System.out.println(getFromSnapshotApi);
+    }
+}
