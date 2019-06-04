c0a0c1e95af02e2f3cb76c8a1afae97c54b256e8
WhoKnows <784925761@qq.com>
2019-06-04 10:43:58 +0800

    验证RandomAccessFile的seek, 之前提交弄丢了

--- JavaTest/src/main/java/com/mytest/io/RandomAccessFileTest.java
+++ JavaTest/src/main/java/com/mytest/io/RandomAccessFileTest.java
@@ -10,12 +10,21 @@ public class RandomAccessFileTest {
 
     @Test
     void test() throws IOException {
-
         try (RandomAccessFile accessFile = new RandomAccessFile(Util.path, "r")) {
             String temp;
-            while ((temp = accessFile.readLine()) != null ) {
+            while ((temp = accessFile.readLine()) != null) {
                 System.out.println(temp);
             }
+        }
+    }
+
+    @Test
+    void append_file_by_seek() throws Exception {
+        try (RandomAccessFile accessFile = new RandomAccessFile(Util.path, "r")) {
+            // 设置文件指针到文件的底部 !!
+            accessFile.seek(accessFile.length());
+            // 这时候再写入, 就是在文件底部补充内容了
+            accessFile.write("hello".getBytes());
         }
     }
 }
