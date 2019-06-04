684704dc0c3dc94f3cb22aab2a18e28b296bc164
WhoKnows <784925761@qq.com>
2019-06-04 11:02:56 +0800

    NIO的性能测试

--- JavaTest/src/main/java/com/mytest/nio/Nio_Is_Faster_Than_IO_Test.java
+++ JavaTest/src/main/java/com/mytest/nio/Nio_Is_Faster_Than_IO_Test.java
@@ -13,9 +13,10 @@ import java.nio.channels.FileChannel;
  *
  * 通过 while(buffer.hasRemaining()) buffer.get() 速度太慢了
  *
- * 通过 buffer.array() 速度很快, 比bio的快一点
+ * 通过 buffer.array() 速度很快, 比bio的快一点, 几十毫秒吧
  *
  * 未测试直接缓冲器
+ * 测试大量写?
  */
 public class Nio_Is_Faster_Than_IO_Test {
     // 17.2M
@@ -26,6 +27,7 @@ public class Nio_Is_Faster_Than_IO_Test {
         long start = System.currentTimeMillis();
 
         FileChannel channel = new FileInputStream(read_file).getChannel();
+        // 修改buffer capacity
         // ByteBuffer buffer = ByteBuffer.allocate(1024);
         // ByteBuffer buffer = ByteBuffer.allocate(5096);
         ByteBuffer buffer = ByteBuffer.allocate(10240);
@@ -34,17 +36,20 @@ public class Nio_Is_Faster_Than_IO_Test {
         int length;
         while ((length = channel.read(buffer)) != -1) {
             buffer.flip();
+            // 1.
             //TODO 通过while buffer.get() 读也太慢了
             // while (buffer.hasRemaining())
             //System.out.print((char) buffer.get());
             // 即使修改为StringBuffer也很慢
             // sb.append((char)buffer.get());
 
+            // 2.
             //TODO 这样也比while buffer.get()快, 和buffer.array()差不多
             // byte[] bytes = new byte[buffer.remaining()];
             // buffer.get(bytes);
             // sb.append(new String(bytes));
 
+            // 3.
             //TODO 通过buffer.array()读
             sb.append(new String(buffer.array(), 0, length));
 
@@ -52,8 +57,8 @@ public class Nio_Is_Faster_Than_IO_Test {
         }
 
         // 通过 while buffer.get(): 35s
-        // 通过 Buffer.array(): 458ms / 501ms / 472ms (buffer: 10m)
-        // 通过 Buffer.array(): 517ms (buffer: 5m)
+        // 通过 Buffer.array(): 458ms / 501ms / 472ms (buffer: 1k)
+        // 通过 Buffer.array(): 517ms (buffer: 5k)
         // 通过 Buffer.array(): 694ms (buffer: 1m)
         // 通过 Buffer.array() + StrinBuffer, buffer: 10m, 花费: 148ms
         System.out.println("时间是: " + (System.currentTimeMillis() - start));
