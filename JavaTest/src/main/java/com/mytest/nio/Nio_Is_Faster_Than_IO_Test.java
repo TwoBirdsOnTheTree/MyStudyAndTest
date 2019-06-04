4b31d3804312239820c3962d81ee4ee447e60036
WhoKnows <784925761@qq.com>
2019-06-04 10:37:59 +0800

    NIO的性能测试

---
+++ JavaTest/src/main/java/com/mytest/nio/Nio_Is_Faster_Than_IO_Test.java
@@ -0,0 +1,73 @@
+package com.mytest.nio;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * System.out.println 是可能拖慢速度的!
+ */
+public class Nio_Is_Faster_Than_IO_Test {
+    // 17.2M
+    private static String read_file = "C:\\Users\\Stark\\Desktop\\test.log";
+
+    @Test
+    void test_read_by_nio_filechannel() throws Exception {
+        long start = System.currentTimeMillis();
+
+        FileChannel channel = new FileInputStream(read_file).getChannel();
+        // ByteBuffer buffer = ByteBuffer.allocate(1024);
+        // ByteBuffer buffer = ByteBuffer.allocate(5096);
+        ByteBuffer buffer = ByteBuffer.allocate(10240);
+
+        StringBuffer sb = new StringBuffer();
+        int length;
+        while ((length = channel.read(buffer)) != -1) {
+            buffer.flip();
+            //TODO 通过while buffer.get() 读也太慢了
+            // while (buffer.hasRemaining())
+            //System.out.print((char) buffer.get());
+            // 即使修改为StringBuffer也很慢
+            // sb.append((char)buffer.get());
+
+            //TODO 这样也比while buffer.get()快, 和buffer.array()差不多
+            // byte[] bytes = new byte[buffer.remaining()];
+            // buffer.get(bytes);
+            // sb.append(new String(bytes));
+
+            //TODO 通过buffer.array()读
+            sb.append(new String(buffer.array(), 0, length));
+
+            buffer.clear();
+        }
+
+        // 通过 while buffer.get(): 35s
+        // 通过 Buffer.array(): 458ms / 501ms / 472ms (buffer: 10m)
+        // 通过 Buffer.array(): 517ms (buffer: 5m)
+        // 通过 Buffer.array(): 694ms (buffer: 1m)
+        // 通过 Buffer.array() + StrinBuffer, buffer: 10m, 花费: 148ms
+        System.out.println("时间是: " + (System.currentTimeMillis() - start));
+    }
+
+    @Test
+    void test_read_by_bufferedreader() throws Exception {
+        try (BufferedReader br = new BufferedReader(new FileReader(read_file))) {
+            long start = System.currentTimeMillis();
+            StringBuffer sb = new StringBuffer();
+
+            String temp;
+            while ((temp = br.readLine()) != null) {
+                // System.out.println(temp);
+                sb.append(temp);
+            }
+
+            // 746ms
+            // 修改为StringBuffer后: 181ms
+            System.out.println("时间是: " + (System.currentTimeMillis() - start));
+        }
+    }
+}
