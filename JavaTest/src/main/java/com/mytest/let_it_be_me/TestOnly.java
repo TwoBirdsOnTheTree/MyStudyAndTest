1888b241416b27b2c856690dbcba19235bff813f
WhoKnows <784925761@qq.com>
2019-06-04 10:44:11 +0800

    测试

--- JavaTest/src/main/java/com/mytest/let_it_be_me/TestOnly.java
+++ JavaTest/src/main/java/com/mytest/let_it_be_me/TestOnly.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,18 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestOnly {
+
+    @Test
+    void test4() {
+        List<String> s = new ArrayList<>(Arrays.asList("a", "b"));
+        List<String> a = new ArrayList<>(Arrays.asList("c", "d"));
+        /**
+         * js 数组拼接方法是: concat, 对应java是addAll
+         * js 使用concat后, 原数组是没有拼接的, 其返回值才是拼接后的结果, 这一点我经常忘
+         * 而java 使用addAll后,
+         */
+        s.addAll(a);
+    }
 
     @Test
     void test3() {
