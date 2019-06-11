48c3638c703a9cc469b16e4324a6758f7f160404
WhoKnows <784925761@qq.com>
2019-06-11 20:47:49 +0800

    Selector

--- JavaTest/src/main/java/com/mytest/nio/SelectorTest.java
+++ JavaTest/src/main/java/com/mytest/nio/SelectorTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 
 /**
  * Selector:
@@ -15,18 +14,31 @@ public class SelectorTest {
     @Test
     void simple_test() throws Exception {
 
-        try (SocketChannel socketChannel = SocketChannel.open()) {
-            socketChannel.connect(new InetSocketAddress("localhost", 801));
+        Selector selector = Selector.open();
 
-            while (!socketChannel.finishConnect()) {
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-                int length;
-                while ((length = socketChannel.read(buffer)) != -1) {
-                    System.out.print(new String(buffer.array(), 0, length));
-                }
-            }
+        //TODO not init
+        SelectableChannel channel = null;
 
-            Selector selector = Selector.open();
-        }
+        // 将SelectableChannel注册到Selector中
+        // 返回选择键(SelectionKey)
+        SelectionKey selectionKey = channel.register(selector, SelectionKey.OP_READ);
+
+    }
+
+
+    @Test
+    void api_test() throws Exception{
+        Selector.open();
+
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        int validOps = channel.validOps();
+        System.out.println(validOps);
+
+        System.out.println();
+        System.out.println(SelectionKey.OP_READ);
+        System.out.println(SelectionKey.OP_WRITE);
+        System.out.println(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+
+         System.out.println(5 | 3);
     }
 }
