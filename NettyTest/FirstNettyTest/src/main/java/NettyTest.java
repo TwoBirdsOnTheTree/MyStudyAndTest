ffe410d15251cc55ec730174c65a3f21b11c21a9
WhoKnows <784925761@qq.com>
2019-07-07 19:20:16 +0800

    Just Test

---
+++ NettyTest/FirstNettyTest/src/main/java/NettyTest.java
@@ -0,0 +1,10 @@
+public class NettyTest {
+    public static void main(String[] args) {
+        NettyTest t = new NettyTest();
+        t.promise_test();
+    }
+
+    void promise_test() {
+        // Promise<Void> promise = new DefaultChannelPromise();
+    }
+}
